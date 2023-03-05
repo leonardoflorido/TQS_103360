238c73040f20277b6ae5eba170e56f364abc9330
Leonardo Flórido <leonardo.s.florido@outlook.com>
2023-03-05 18:00:48 +0000

    Exercise 2 done.

--- lab3/lab3_2cars_3integration/src/main/java/tqs/cars/services/CarManagerService.java
+++ lab3/lab3_2cars_3integration/src/main/java/tqs/cars/services/CarManagerService.java
@@ -28,6 +28,10 @@ public class CarManagerService {
     }
 
     public Optional<Car> getCarDetails(Long carId) {
-        return Optional.of(carRepository.findByCarId(carId));
+        Car car = carRepository.findByCarId(carId);
+        if (car == null) {
+            return Optional.empty();
+        }
+        return Optional.of(car);
     }
 }
